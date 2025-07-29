244c0eb1fa5770a3aa3fbbfea4a83263ad4e4287
vruizz9 <vivi.ruiz1000@gmail.com>
2025-07-29 10:19:46 -0500

    Interfaz del login (diseño incompleto)

--- Blog_Con_Roles_De_Usuario_Y_Comentarios/src/app/app.routes.ts
+++ Blog_Con_Roles_De_Usuario_Y_Comentarios/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './funcionalidades/autenticacion/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
--- Blog_Con_Roles_De_Usuario_Y_Comentarios/src/app/funcionalidades/autenticacion/login/login.ts
+++ Blog_Con_Roles_De_Usuario_Y_Comentarios/src/app/funcionalidades/autenticacion/login/login.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
-export class Login {
-
-}
+export class LoginComponent {}
